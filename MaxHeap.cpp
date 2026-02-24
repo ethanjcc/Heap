590263699b47d0ea9115ea8b37528c5e7697725a
U-BSD\453370 <453370@SHS-HYBF6X3.beaverton.k12.or.us>
2026-02-24 13:27:09 -0800

    second commit

--- MaxHeap.cpp
+++ MaxHeap.cpp
@@ -1,6 +1,8 @@
 #include "MaxHeap.h"
 #include <algorithm>
-//#include <iostream>
+#include <iostream>
+
+using namespace std;
 
 MaxHeap::MaxHeap() {
   size = 0;
@@ -14,7 +16,51 @@ void MaxHeap::insert(int value){
   int i = size;
   //check for if it is larger than the parent
   while (i > 1 && heap[i] > heap[i/2]) {
-    std::swap(heap[i], heap[i/2]);
+    swap(heap[i], heap[i/2]);
     i = i/2;
   }
+}
+
+void MaxHeap::print(){
+  for (int i = 1; i <= size; i++) {
+    cout << heap[i] << " ";
+  }
+  cout << endl;   
+}
+
+int MaxHeap::removeMax(){
+  if (size == 0){
+    cout << "error" << endl;
+    return 0;
+  }
+  //get root then move last number to root and remove 
+  int maxValue = heap[1];
+  heap[1] = heap[size];
+  size--;
+  int i = 1;
+  int running = 1;
+  while (running == 1) {
+    int left = i * 2;
+    int right = i * 2 + 1;
+    //stop if there are no children
+    if (left > size) {
+      break;
+    }
+    //find a larger child
+    int larger = left;
+    if (right <= size && heap[right] > heap[left]){
+      larger = right;
+    }
+    //stop if parent is larger
+    if (heap[i] >= heap[larger]) {
+      break;
+    }
+    //if not then keep going
+    else {
+      swap(heap[i], heap[larger]);
+      i = larger;
+    }
+  }
+  cout << maxValue << endl;
+  return maxValue;
 }
--- heap.cpp
+++ heap.cpp
@@ -6,8 +6,33 @@ using namespace std;
 
 int main() {
   MaxHeap h;
-  h.insert(90);
-  h.insert(70);
-  h.insert(80);
+  char input[20];
+  int going = 1;
+  while (going == 1) {
+    cout << "what do you want" << endl;
+    cin >> input;
+
+    if (strcmp(input, "ADD") == 0) {
+      int value;
+      cout << "what number: " << endl;
+      cin >> value;
+      h.insert(value);
+    }
+    else if (strcmp(input, "PRINT") == 0){
+      h.print();
+    }
+    else if (strcmp(input, "DELMAX") == 0) {
+      int removed = h.removeMax();
+      if (removed = -1) {
+	cout << "its empty" << endl;
+      }
+      else {
+	cout << "removed" << endl;
+      }
+    }
+    else if (strcmp(input, "QUIT") == 0) {
+      return 0;
+    }
+  }
   return 0;
 }
